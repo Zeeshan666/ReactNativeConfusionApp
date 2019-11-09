9078feb5c88cf7c4e846300e196db9b3c64901e8
zeeshan666 <zeeshanmanzoor.zm@gmail.com>
2019-11-09 08:49:37 -0800

    navigation end 2

--- Components/Home.js
+++ Components/Home.js
@@ -2,6 +2,14 @@ import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: '',
+      promotion: '',
+      Leader: '',
+    };
+  }
   render() {
     return (
       <View>
